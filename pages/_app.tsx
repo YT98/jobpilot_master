023f171a288681934f50ede158c5830d937ce957
Yannis Tarfa <yannistarfa98@gmail.com>
2023-03-31 12:56:58 -0400

    Added tailwindcss and basic styling for sign in

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import '../styles/index.css'
 import { AppProps } from 'next/app';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
--- pages/signin.tsx
+++ pages/signin.tsx
@@ -33,62 +33,82 @@ const SignIn: React.FC = () => {
     };
 
 	// Handle the form submission
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    try {
-      const response = await fetch('http://localhost:5000/user/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email: state.email,
-          password: state.password,
-        }),
-      });
-	  // If the response is not ok, throw an error
-      if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(errorData.error);
-      }
-      const data = await response.json();
-	  // Store the token in session storage
-      sessionStorage.setItem('token', data.token);
-	  // Redirect to the dashboard
-	  router.push('/dashboard');
-    } catch (error) {
-		// TODO: Fix type error
-		// If there is an error, set the error state
-        setState({ ...state, error: error.message });
-    }
-  };
+	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		try {
+			const response = await fetch('http://localhost:5000/user/login', {
+				method: 'POST',
+				headers: {
+				'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+				email: state.email,
+				password: state.password,
+				}),
+			});
+			// If the response is not ok, throw an error
+			if (!response.ok) {
+				const errorData = await response.json();
+				throw new Error(errorData.error);
+			}
+			const data = await response.json();
+			// Store the token in session storage
+			sessionStorage.setItem('token', data.token);
+			// Redirect to the dashboard
+			router.push('/dashboard');
+		} catch (error) {
+			// TODO: Fix type error
+			// If there is an error, set the error state
+			setState({ ...state, error: error.message });
+		}
+	};
 
   return (
-    <div>
-      <h1>Sign In</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            value={state.email}
-            onChange={handleEmailChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={state.password}
-            onChange={handlePasswordChange}
-          />
-        </div>
-        <p>{state.error}</p>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+	<div className="bg-sky-100 grid h-screen place-items-center">
+		<div className="w-full max-w-xs">
+			<form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+				<h1 className="block text-gray-700 text-lg font-bold mb-2"> Sign In </h1>
+				<div className="mb-4">
+					<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+						Email
+					</label>
+					<input 
+						className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+						type="email"
+						id="email"
+						value={state.email}
+						onChange={handleEmailChange}
+					/>
+				</div>
+				<div className="mb-6">
+					<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+						Password
+					</label>
+					<input 
+						className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+						type="password"
+						id="password"
+						value={state.password}
+						onChange={handlePasswordChange}
+					/>
+				</div>
+				<div className="flex items-center justify-between">
+					<button 
+						className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
+						type="submit"
+					>
+						Sign In
+					</button>
+					<a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+						Forgot Password?
+					</a>
+				</div>
+			</form>
+			<p className="text-center text-gray-500 text-xs">
+				&copy;2023 []. All rights reserved.
+			</p>
+		</div>
+	</div>
   );
 };
 
