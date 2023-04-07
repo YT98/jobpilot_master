3d1a8d01e142f677548ef905459ae52373cf751b
Yannis Tarfa <yannistarfa98@gmail.com>
2023-04-06 23:09:11 -0400

    Removed hard/soft skill properties

--- jobpilot_back/models/Skill.py
+++ jobpilot_back/models/Skill.py
@@ -8,10 +8,6 @@ from app import db
 class Skill(db.Model, Interface):
     id: int
     name: str
-    soft_skill: bool
-    hard_skill: bool
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
-    soft_skill = db.Column(db.Boolean, nullable=True)
-    hard_skill = db.Column(db.Boolean, nullable=True)
